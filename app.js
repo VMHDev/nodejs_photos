e7095a945c56e7f1458f4711c7ffd8f54dd00496
VMHDev <vmhpublic2017@gmail.com>
2021-05-14 10:11:52 +0700

    Add cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 
 const connectDB = require('./config/db');
 const authRouter = require('./routes/auth');
@@ -8,6 +9,7 @@ const userRouter = require('./routes/users');
 // Start app
 const app = express();
 app.use(express.json()); // express accept post body with json
+app.use(cors());
 
 // Connect database
 connectDB();
